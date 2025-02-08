d67134d299817ee457eab3229dc23453ca4ed680
Martin Helmich <m.helmich@mittwald.de>
2025-02-08 16:41:21 +0100

    fix: change instance key to "inst" ("ctx" does not make sense)

--- pkg/domain/model/session.go
+++ pkg/domain/model/session.go
@@ -83,7 +83,7 @@ func (s *SessionClaims) MarshalJSON() ([]byte, error) {
 		"fname": s.Session.FirstName,
 		"lname": s.Session.LastName,
 		"email": s.Session.Email,
-		"ctx":   s.Session.Instance.Context,
+		"inst":  s.Session.Instance.Context,
 	}
 
 	return json.Marshal(out)
